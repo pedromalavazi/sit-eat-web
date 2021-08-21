c29b40fb062a0296b113368ebd6355c9159da654
Rafael Rodrigues <rrv.rafael@outlook.com>
2021-08-21 02:28:58 -0300

    ajustes

--- lib/pages/admin/restaurant_profile_page.dart
+++ lib/pages/admin/restaurant_profile_page.dart
@@ -1,17 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sit_eat_web/app/controller/restaurant_register_controller.dart';
+import 'package:sit_eat_web/app/controller/restaurant_profile_controller.dart';
+import 'package:sit_eat_web/app/routes/app_pages.dart';
 
-class ProfilePage extends GetView<RestaurantRegisterController> {
-  final RestaurantRegisterController _registerRestaurantController =
-      Get.put(RestaurantRegisterController());
+class ProfilePage extends GetView<RestaurantProfileController> {
+  final RestaurantProfileController _restaurantProfileController =
+      Get.put(RestaurantProfileController());
   final _formKey = GlobalKey<FormState>();
   int count = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text("data"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountEmail: Text("user@mail.com"),
+              accountName: Text("Seu zé"),
+              currentAccountPicture: CircleAvatar(
+                child: Text("SZ"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Minha conta"),
+              onTap: () {
+                Get.toNamed(Routes.REGISTER_RESTAURANT);
+                // Navigator.pop(context);
+                //Navegar para outra página
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.shopping_basket),
+              title: Text("Meus pedidos"),
+              onTap: () {
+                Navigator.pop(context);
+                //Navegar para outra página
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text("Favoritos"),
+              onTap: () {
+                Navigator.pop(context);
+                //Navegar para outra página
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -12,160 +12,167 @@ class LoginPage extends GetView<LoginController> {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       body: Center(
-        child: Container(
-          width: 400.0,
-          height: 420.0,
-          child: Card(
-            elevation: 15.0,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  Row(
+        child: Wrap(
+          children: [
+            Container(
+              width: 400.0,
+              height: 420.0,
+              child: Card(
+                elevation: 15.0,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
                     children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(45, 25, 0, 30),
-                        child: SizedBox(
-                          width: 50,
-                          height: 50,
-                          child: Image.asset("assets/logo-only.png"),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(10, 5, 0, 0),
-                        child: Text(
-                          "Sit & Eat",
-                          style: TextStyle(
-                            fontSize: 23.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(45, 20, 0, 0),
-                        width: 300.0,
-                        height: 40.0,
-                        child: TextField(
-                          controller: _loginController.emailTextController,
-                          obscureText: false,
-                          autofocus: true,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Usuário',
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(45, 23, 0, 0),
-                        width: 300.0,
-                        height: 40.0,
-                        child: TextField(
-                          controller: _loginController.passwordTextController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Senha',
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(32, 0, 0, 0),
-                        child: Obx(
-                          () => Switch(
-                            value: _loginController.stayLogged.value,
-                            onChanged: 0 == 1
-                                ? null
-                                : (bool value) {
-                                    _loginController.stayLogged.value = value;
-                                  },
-                          ),
-                        ),
-                      ),
-                      Text(
-                        'Mantenha-me conectado',
-                        style: TextStyle(
-                          fontSize: 15,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(40, 5, 0, 0),
-                        width: 135.0,
-                        height: 20.0,
-                        child: TextButton(
-                          onPressed: () {
-                            Get.toNamed(Routes.REGISTER_RESTAURANT);
-                          },
-                          child: Text(
-                            "Esqueceu a senha?",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(45, 25, 0, 0),
-                        width: 300.0,
-                        height: 40.0,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            _loginController.login();
-                          },
-                          child: Text(
-                            "Login",
-                            style: TextStyle(fontSize: 20.0),
-                          ),
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.resolveWith(
-                                (states) => Colors.red),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(150, 10, 0, 0),
-                        width: 90.0,
-                        height: 20.0,
-                        child: TextButton(
-                          onPressed: () {
-                            Get.toNamed(Routes.REGISTER_RESTAURANT);
-                          },
-                          child: Text(
-                            "Cadastrar-se",
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 15.0,
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(45, 25, 0, 30),
+                            child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Image.asset("assets/logo-only.png"),
                             ),
                           ),
-                        ),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(10, 5, 0, 0),
+                            child: Text(
+                              "Sit & Eat",
+                              style: TextStyle(
+                                fontSize: 23.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(45, 20, 0, 0),
+                            width: 300.0,
+                            height: 40.0,
+                            child: TextField(
+                              controller: _loginController.emailTextController,
+                              obscureText: false,
+                              autofocus: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Usuário',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(45, 23, 0, 0),
+                            width: 300.0,
+                            height: 40.0,
+                            child: TextField(
+                              controller:
+                                  _loginController.passwordTextController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Senha',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(32, 0, 0, 0),
+                            child: Obx(
+                              () => Switch(
+                                value: _loginController.stayLogged.value,
+                                onChanged: 0 == 1
+                                    ? null
+                                    : (bool value) {
+                                        _loginController.stayLogged.value =
+                                            value;
+                                      },
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'Mantenha-me conectado',
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(40, 5, 0, 0),
+                            width: 135.0,
+                            height: 20.0,
+                            child: TextButton(
+                              onPressed: () {
+                                Get.toNamed(Routes.REGISTER_RESTAURANT);
+                              },
+                              child: Text(
+                                "Esqueceu a senha?",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(45, 25, 0, 0),
+                            width: 300.0,
+                            height: 40.0,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                _loginController.login();
+                              },
+                              child: Text(
+                                "Login",
+                                style: TextStyle(fontSize: 20.0),
+                              ),
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.resolveWith(
+                                        (states) => Colors.red),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(150, 10, 0, 0),
+                            width: 90.0,
+                            height: 20.0,
+                            child: TextButton(
+                              onPressed: () {
+                                Get.toNamed(Routes.REGISTER_RESTAURANT);
+                              },
+                              child: Text(
+                                "Cadastrar-se",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
