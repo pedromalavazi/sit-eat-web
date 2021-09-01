841e0e203e8277b91808afe40c2a15e9246f870f
Rafael Rodrigues <rrv.rafael@outlook.com>
2021-08-31 21:40:41 -0300

    updates

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,15 +4,15 @@ import 'package:sit_eat_web/app/binding/restaurant_register_binding.dart';
 import 'package:sit_eat_web/app/binding/restaurant_approval_binding.dart';
 import 'package:sit_eat_web/app/binding/restaurant_manage_binding.dart';
 import 'package:sit_eat_web/app/binding/restaurant_profile_binding.dart';
-import 'package:sit_eat_web/app/binding/register_restaurant_binding.dart';
+import 'package:sit_eat_web/app/binding/table_binding.dart';
 import 'package:sit_eat_web/pages/admin/restaurant_approve_page.dart';
 import 'package:sit_eat_web/pages/admin/restaurant_management_page.dart';
-import 'package:sit_eat_web/pages/admin/restaurant_profile_page.dart';
+import 'package:sit_eat_web/pages/restaurant/profile/restaurant_profile_page.dart';
 import 'package:sit_eat_web/pages/home_page.dart';
 import 'package:sit_eat_web/pages/login/login_page.dart';
 import 'package:sit_eat_web/pages/register/product/product_register_page.dart';
 import 'package:sit_eat_web/pages/register/restaurant/restaurant_register_page.dart';
-import 'package:sit_eat_web/pages/register/restaurant/register_restaurant_page.dart';
+import 'package:sit_eat_web/pages/restaurant/tables/tables_page.dart';
 part './app_routes.dart';
 
 class AppPages {
--- lib/pages/restaurant/profile/restaurant_profile_page.dart
+++ lib/pages/restaurant/profile/restaurant_profile_page.dart
@@ -1,4 +1,3 @@
-import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
