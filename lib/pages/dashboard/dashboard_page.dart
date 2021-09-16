3e19a4a9d195cb191d7cc23da81af6c064a878fe
Rafael Rodrigues <rrv.rafael@outlook.com>
2021-09-16 18:35:43 -0300

    correção do dashboard
    Removi o datatable e gerei uma lista de cards.

--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:sit_eat_web/app/controller/dashboard_controller.dart';
 import 'package:sit_eat_web/pages/tables/widgets/table_order_card.dart';
 import 'package:sit_eat_web/utils/menu.dart';
-import 'package:sit_eat_web/utils/web_utils.dart';
 
 class DashboardPage extends GetView<DashboardController> {
   final DashboardController _dashboardController =
@@ -20,29 +19,6 @@ class DashboardPage extends GetView<DashboardController> {
                 TableOrderCard(table: _dashboardController.tables[index]),
           ),
         ),
-        //     GridView.builder(
-        //   itemCount: _dashboardController.tables.length, //Quantidade de Mesas
-
-        //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        //     childAspectRatio: 2.0,
-        //     crossAxisCount: 4,
-        //     crossAxisSpacing: 15,
-        //   ),
-
-        //   itemBuilder: (BuildContext context, int index) {
-        //     return LayoutBuilder(
-        //       builder: (context, constraints) {
-        //         double font =
-        //             fontSize(constraints.maxHeight * 0.15, min: 9, max: 22);
-        //         // Card Mesas
-        //         return TableOrderCard(
-        //           table: _dashboardController.tables[index],
-        //           font: font,
-        //         );
-        //       },
-        //     );
-        //   },
-        // ),
       ),
     );
   }
--- lib/pages/tables/widgets/table_order_card.dart
+++ lib/pages/tables/widgets/table_order_card.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sit_eat_web/app/controller/dashboard_controller.dart';
-import 'package:sit_eat_web/app/data/model/order_model.dart';
 import 'package:sit_eat_web/app/data/model/table_model.dart';
-import 'package:sit_eat_web/pages/tables/widgets/orders.dart';
 
 class TableOrderCard extends GetView<DashboardController> {
   final DashboardController _dashboardController =
@@ -11,15 +9,9 @@ class TableOrderCard extends GetView<DashboardController> {
 
   final TableModel table;
   TableOrderCard({required this.table});
-  List<OrderModel> orders = [];
-
-  // final double font;
-  // TableOrderCard({required this.table, required this.font});
 
   @override
   Widget build(BuildContext context) {
-    // final columns = ["id", "orderTime", "productId", "quantity", "total"];
-    // final columns = ["Number", "Capacity", "Busy"];
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Container(
@@ -38,50 +30,92 @@ class TableOrderCard extends GetView<DashboardController> {
                   content: Container(
                     width: 500,
                     height: 500,
-                    child:
-                        // DataTable(
-                        //   columns: getColumns(columns),
-                        //   rows: getRows(teste),
-                        // ),
-
-                        // Wrap(
-                        //   children: List.generate(
-                        //     _dashboardController.tables.length,
-                        //     (index) =>
-                        //         Order(order: _dashboardController.tables[index]),
-                        //   ),
-                        // ),
-                        ListView(
-                      children: [
-                        PaginatedDataTable(
-                          columns: [
-                            DataColumn(label: Text('Produto')),
-                            DataColumn(label: Text('Quantidade')),
-                            DataColumn(label: Text('Preço')),
-                            DataColumn(label: Text('Status')),
-                          ],
-                          source: _DataSource(context, table),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Text("Confirmar pedido"),
-                        ),
-                      ],
+                    child: SingleChildScrollView(
+                      child: Wrap(
+                        children: [
+                          Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    "Produto",
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Quantidade",
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Preço",
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Status",
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: List.generate(
+                              20,
+                              (index) => Container(
+                                margin: EdgeInsets.fromLTRB(0, 25, 0, 0),
+                                width: 500,
+                                height: 60,
+                                child: Card(
+                                  elevation: 2.0,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text(
+                                        "Produto",
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                        ),
+                                      ),
+                                      Text(
+                                        "Quantidade",
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                        ),
+                                      ),
+                                      Text(
+                                        "Preço",
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                        ),
+                                      ),
+                                      Text(
+                                        "Status",
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-
-                    // Column(
-                    //   children: [)
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    //   children: [
-                    //     Text("Produto"),
-                    //     Text("Quantidade"),
-                    //     Text("Preço"),
-                    //     Text("Status"),
-                    //   ],
-                    // ),
-                    // ],
-                    // ),
                   ),
                 ),
               );
@@ -129,93 +163,4 @@ class TableOrderCard extends GetView<DashboardController> {
       ),
     );
   }
-
-  List<DataColumn> getColumns(List<String> columns) => columns
-      .map((String column) => DataColumn(
-            label: Text(column),
-          ))
-      .toList();
-  List<DataRow> getRows(List<TableModel> teste) => teste
-      .map((TableModel tableModel) => DataRow(cells: [
-            DataCell(Text(tableModel.number.toString())),
-            DataCell(Text(tableModel.capacity.toString())),
-            DataCell(Text(tableModel.busy.toString()))
-          ]))
-      .toList();
-  // List<DataRow> getRows(List<OrderModel> orders) => orders
-  //     .map((OrderModel orderTable) => DataRow(cells: [
-  //           DataCell(Text(orderTable.id.toString())),
-  //           DataCell(Text(orderTable.orderTime.toString())),
-  //           DataCell(Text(orderTable.productId.toString())),
-  //           DataCell(Text(orderTable.quantity.toString())),
-  //           DataCell(Text(orderTable.total.toString())),
-  //         ]))
-  //     .toList();
-}
-
-class _Row {
-  _Row(
-    this.valueA,
-    this.valueB,
-    this.valueC,
-    this.valueD,
-  );
-
-  final String valueA;
-  final String valueB;
-  final double valueC;
-  final int valueD;
-
-  bool selected = false;
-}
-
-class _DataSource extends DataTableSource {
-  final DashboardController _dashboardController =
-      Get.find<DashboardController>();
-  final TableModel table;
-
-  _DataSource(this.context, this.table) {
-    _rows = <_Row>[
-      _Row("Produto", 'CellB1', 5.8, 1),
-    ];
-  }
-
-  final BuildContext context;
-  List<_Row> _rows = [];
-
-  int _selectedCount = 0;
-
-  @override
-  DataRow? getRow(int index) {
-    assert(index >= 0);
-    if (index >= _rows.length) return null;
-    final row = _rows[index];
-    return DataRow.byIndex(
-      index: index,
-      selected: row.selected,
-      onSelectChanged: (value) {
-        if (row.selected != value) {
-          _selectedCount += value! ? 1 : -1;
-          assert(_selectedCount >= 0);
-          row.selected = value;
-          notifyListeners();
-        }
-      },
-      cells: [
-        DataCell(Text(row.valueA)),
-        DataCell(Text(row.valueB)),
-        DataCell(Text(row.valueC.toString())),
-        DataCell(Text(row.valueD.toString())),
-      ],
-    );
-  }
-
-  @override
-  int get rowCount => _rows.length;
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get selectedRowCount => _selectedCount;
 }
