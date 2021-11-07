14702cbf34524f91c512c2e7aec3365c0314d948
rrv-rafael <rrv.rafael@outlook.com>
2021-11-06 22:50:15 -0300

    front lista de espera

--- lib/pages/waitinglist/waiting_list_page.dart
+++ lib/pages/waitinglist/waiting_list_page.dart
@@ -22,72 +22,62 @@ class WaitingListPage extends GetView<TablesController> {
   _body(BuildContext context) {
     return Container(
       child: SingleChildScrollView(
-        child: Wrap(
+        child: Column(
           children: [
-            Column(
-              children: <Widget>[
-                Wrap(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          "Posição",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "Nome",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "Horário da solicitação",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "Quantidade de pessoas na mesa",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "Telefone",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        // Wrap(
-                        //   children: [
-                        //     Text("Posição Nome Horário da solicitação"),
-                        //   ],
-                        // ),
-                      ],
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Obx(
-                    () => Wrap(
-                      children: List.generate(
-                        _tableController.tables.length,
-                        (index) => Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Container(
-                            height: 45.0,
-                            color: index % 2 == 0 ? Color(0xFFe0e0e0) : Color(0xFFbcbcbc),
-                            child: ListData(
-                              table: _tableController.tables[index],
-                            ),
-                          ),
-                        ),
-
-                        // Padding(
-                        //   padding: const EdgeInsets.all(2.0),
-                        //   child: Container(
-                        //     color: index % 2 == 0 ? Color(0xFFe0e0e0) : Color(0xFFbcbcbc),
-
-                        //   ),
-                        // ),
+            Wrap(
+              children: [
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                //   children: [
+                //     Text(
+                //       "Posição",
+                //       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10.0),
+                //     ),
+                //     Text(
+                //       "Nome",
+                //       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10.0),
+                //     ),
+                //     Text(
+                //       "Horário da solicitação",
+                //       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10.0),
+                //     ),
+                //     Text(
+                //       "Quantidade de pessoas na mesa",
+                //       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10.0),
+                //     ),
+                //     Text(
+                //       "Telefone",
+                //       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10.0),
+                //     ),
+                //   ],
+                // ),
+                Obx(
+                  () => Wrap(
+                    children: List.generate(
+                      _tableController.tables.length,
+                      (index) => ListData(
+                        table: _tableController.tables[index],
                       ),
                     ),
                   ),
                 ),
+                // Obx(
+                //   () => Wrap(
+                //     children: List.generate(
+                //       _tableController.tables.length,
+                //       (index) => Padding(
+                //         padding: const EdgeInsets.all(4.0),
+                //         child: Container(
+                //           height: 45.0,
+                //           color: index % 2 == 0 ? Color(0xFFe0e0e0) : Color(0xFFbcbcbc),
+                //           child: ListData(
+                //             table: _tableController.tables[index],
+                //           ),
+                //         ),
+                //       ),
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           ],
--- lib/pages/waitinglist/widgets/list_data.dart
+++ lib/pages/waitinglist/widgets/list_data.dart
@@ -9,129 +9,67 @@ class ListData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      children: [
-        Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("Posição"),
-              Text("Nome"),
-              Text("horário da solicitação"),
-              Text("Quantidade de pessoas na mesa"),
-              Text("Telefone"),
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          DataTable(
+            columns: [
+              DataColumn(
+                label: Text(""),
+              ),
+              DataColumn(
+                label: Text(""),
+              ),
+              DataColumn(
+                label: Text(""),
+              ),
+              DataColumn(
+                label: Text(""),
+              ),
+              DataColumn(
+                label: Text(""),
+              ),
+              // DataColumn(
+              //   label: Text("Posição"),
+              // ),
+              // DataColumn(
+              //   label: Text("Nome"),
+              // ),
+              // DataColumn(
+              //   label: Text("Horário da reserva"),
+              // ),
+              // DataColumn(
+              //   label: Text("Quantidade de pessoas na mesa"),
+              // ),
+              // DataColumn(
+              //   label: Text("Telefone"),
+              // ),
+            ],
+            rows: [
+              DataRow(
+                cells: [
+                  DataCell(
+                    Text("${table.number}"),
+                  ),
+                  DataCell(
+                    Text("Quantidade: ${table.capacity} pessoas"),
+                  ),
+                  DataCell(
+                    Text("Nome ${table.number}"),
+                  ),
+                  DataCell(
+                    Text("Horário: ${table.capacity}"),
+                  ),
+                  DataCell(
+                    Text("Tefone: ${table.capacity}"),
+                  ),
+                ],
+              ),
             ],
           ),
-        ),
-        Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("01"),
-              Text("02"),
-              Text("03"),
-              Text("04"),
-              Text("05"),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
-
-    // Wrap(
-    //   children: [
-    //     Row(
-    //       mainAxisAlignment: MainAxisAlignment.spaceAround,
-    //       children: [
-    //         // SizedBox(width: 130.0),
-    //         Padding(
-    //           padding: const EdgeInsets.all(8.0),
-    //           child: Column(
-    //             children: [
-    //               Container(
-    //                 color: Colors.red,
-    //                 width: 40,
-    //                 child: Text(
-    //                   "100",
-    //                   style: TextStyle(fontWeight: FontWeight.bold),
-    //                 ),
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    //         // SizedBox(width: 150.0),
-    //         Padding(
-    //           padding: const EdgeInsets.all(8.0),
-    //           child: Column(
-    //             children: [
-    //               Container(
-    //                 color: Colors.blue,
-    //                 width: 350,
-    //                 child: Text(
-    //                   "Rafael Rodrigues Vitor",
-    //                   style: TextStyle(fontWeight: FontWeight.bold),
-    //                 ),
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    //         // SizedBox(width: 105.0),
-    //         Padding(
-    //           padding: const EdgeInsets.all(8.0),
-    //           child: Column(
-    //             children: [
-    //               Container(
-    //                 color: Colors.green,
-    //                 width: 60,
-    //                 child: Text(
-    //                   "10:20",
-    //                   style: TextStyle(fontWeight: FontWeight.bold),
-    //                 ),
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    //         // SizedBox(width: 450.0),
-    //         Padding(
-    //           padding: const EdgeInsets.all(8.0),
-    //           child: Column(
-    //             children: [
-    //               Container(
-    //                 color: Colors.yellow,
-    //                 width: 20,
-    //                 child: Text(
-    //                   "5",
-    //                   style: TextStyle(fontWeight: FontWeight.bold),
-    //                 ),
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    //         // SizedBox(width: 330.0),
-    //         Padding(
-    //           padding: const EdgeInsets.all(8.0),
-    //           child: Column(
-    //             children: [
-    //               Container(
-    //                 color: Colors.pink,
-    //                 width: 170,
-    //                 child: Text(
-    //                   "(19) 99214-3881",
-    //                   style: TextStyle(fontWeight: FontWeight.bold),
-    //                 ),
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    // Icon(Icons.label),
-    // Text("Mesa ${table.number}"),
-    // Text("Quantidade: ${table.capacity} pessoas"),
-    // Text("Mesa ${table.number}"),
-    // Text("Quantidade: ${table.capacity} pessoas"),
-    // Text("Quantidade: ${table.capacity} pessoas"),
-    // ],
-    // ),
-    // ],
-    // );
   }
 }
